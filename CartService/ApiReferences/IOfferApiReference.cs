ceb6305c6448db566b7075cd69dbdda0d819a478
konprok <konradprokop20@gmail.com>
2024-06-18 03:35:38 +0200

    Get count added

--- CartService/ApiReferences/IOfferApiReference.cs
+++ CartService/ApiReferences/IOfferApiReference.cs
@@ -5,7 +5,6 @@ namespace CartService.ApiReferences;
 
 public interface IOfferApiReference
 {
-    // referencja do pobierania ofert po ich id
     [Post("/offers")]
     Task<List<OffersResponse>> GetOffersByIds([Body] List<long> offerIds);
 }
